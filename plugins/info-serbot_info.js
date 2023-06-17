ca250858f0d9256dae45ca5fd0e28e152227ea0f
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-17 08:43:27 -0600

    Update info-serbot_info.js

--- plugins/info-serbot_info.js
+++ plugins/info-serbot_info.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
 let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]  
-const message = users.map(v => '🔰 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=Bot\n(${v.name})\n\n`).join('\n')
+const message = users.map(v => '🥏 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}estado\n(${v.name})\n\n`).join('\n')
 const replyMessage = (message.length === 0) ? "*⚠️ NO HAY SUB BOTS DISPONIBLE. VERIFIQUE MÁS TARDE.*" : message
 await m.reply( '*🚀 AQUÍ TIENE LA LISTA DE LOS SUBBOTS ACTIVÓS*')
 await m.reply(replyMessage.trim())}
