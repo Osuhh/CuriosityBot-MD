03d3144eb3a0bf398d5f0821a54a3579cfecf081
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-17 12:46:07 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -69,15 +69,13 @@ let td = `${pickRandom([d1,d2,d3,d4,d5,d6])}`
     }
     }
     await conn.reply(m.chat, '*Enviando el menu . . .*', ftrol) 
-m.react('📌')     
+m.react('🚀')     
 let menu = `*❏──── 「 MENU 」 ────❏*
 
 *U S U A R I O*
 *Nombre:* ${name}
 *Nivel:* ${level}
-*Coins:* ${money}
 *Exp:* ${exp}
-*Diamantes:* ${limit}
 
 *H O Y*
 *Fecha:* ${date}
