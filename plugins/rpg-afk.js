8b4c0d9eab61e9a0538938be52d045907a32d2ea
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-24 14:48:23 -0600

    Update rpg-afk.js

--- plugins/rpg-afk.js
+++ plugins/rpg-afk.js
@@ -1,9 +1,20 @@
-let handler = async (m, { text }) => {
-let user = global.db.data.users[m.sender]
-user.afk = + new Date
-user.afkReason = text
-m.reply(`╭──╼┥𝐀𝐅𝐊┝╾──╮\n╽ ┌──────────┐ ┃\n${conn.getName(m.sender)} estara Inactivo\n┃ ├──────────┤ ┃\nPor el motivo: ${text ? ': ' + text : ''}\n╿ └──────────┘ ╿\n╰─┥𝐀𝐅𝐊┝╾─╯\n\n*「 CURIOSITY-AFK 」 ${conn.getName(m.sender)} 𝙴𝚂𝚃𝙰𝚁𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺), 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙽𝙾 𝙻𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴𝙽*\n\n*—◉ 𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙻𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺)${text ? ': ' + text : ''}*`)}
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
-handler.command = /^afk$/i
+import db from '../lib/database.js'
+let handler = async (m, { conn, text }) => {
+    let user = global.db.data.users[m.sender]
+    user.afk = + new Date
+    user.afkReason = text
+    conn.reply(m.chat, `
+\t\t\t\t *「 ⏰ ESTADO AFK ⏰ 」*
+ 
+*► Estarás afk hasta que envíes un mensaje.*
+
+👤 *Usuario:* @${m.sender.split`@`[0]} 
+👀 *Razón:* ${text ? ': ' + text : ''}
+  `, m, { mentions: [m.sender]})
+}
+handler.help = ['afk *<razón>*']
+handler.tags = ['fun']
+handler.command = ['afk']
+handler.register = true
+
 export default handler
