9429c4f05b1247fc447c7f0b46955a1f18563f6f
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-25 10:38:55 -0600

    Update rpg-minar3.js

--- plugins/rpg-minar3.js
+++ plugins/rpg-minar3.js
@@ -30,3 +30,9 @@ hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 hours = (hours < 10) ? "0" + hours : hours
 minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
+
+return minutes + " m y " + seconds + " s " 
+}  
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
