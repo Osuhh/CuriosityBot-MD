17ce999e80456bd4ff92bc06ac87d7563127876e
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-26 12:22:27 -0600

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -1,18 +1,15 @@
-
-//import db from '../lib/database.js'
-
 const free = 5000
 const prem = 20000
 
 let handler = async (m, {conn, isPrems }) => {
   let time = global.db.data.users[m.sender].lastclaim + 3600000
-  if (new Date - global.db.data.users[m.sender].lastclaim < 3600000) throw `🎁 *ʏᴀ ʀᴇᴄᴏɢɪsᴛᴇ ᴛᴜ ʀᴇᴄᴏᴍᴘᴇɴsᴀ*\n\n🕚 ᴠᴜᴇʟᴠᴇ ᴇɴ *${msToTime(time - new Date())}* `
+  if (new Date - global.db.data.users[m.sender].lastclaim < 3600000) throw `*🕚 Vuelve en  *${msToTime(time - new Date())}* `
   global.db.data.users[m.sender].exp += isPrems ? prem : free
   m.reply(`
-🎁 *ʀᴇᴄᴏᴍᴘᴇɴsᴀ ᴅɪᴀʀɪᴀ*
+🎁 *RECOMPENSA DIARIA*
 
-🔸 *ʜᴀs ʀᴇᴄɪʙɪᴅᴏ:*
-🆙 *xᴘ* : +${isPrems ? prem : free}`)
+* Has recibido:*
+🆙 *XP* : +${isPrems ? prem : free}`)
   global.db.data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily']
