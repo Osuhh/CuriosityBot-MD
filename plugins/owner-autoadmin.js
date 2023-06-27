262f7928d8eab40dc4126c11b5a5ae1a68b612a3
LoliBot <137656601+undefinidoo@users.noreply.github.com>
2023-06-27 09:24:40 -0300

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,8 +1,6 @@
-/* Creditos a https://github.com/unptoadrih15/UPABOT-MD */
-
 let handler = async (m, { conn, isAdmin }) => {  
 if (m.fromMe) return
-if (isAdmin) throw '*[❗] 𝙷𝙾𝙻𝙰 𝙲𝚁𝙴𝙰𝙳𝙾𝚁, 𝙲𝙾𝙼𝙾 𝙴𝚂𝚃𝙰? 𝚄𝚂𝚃𝙴𝙳 𝚈𝙰 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*'
+if (isAdmin) throw '*HOLA CREADOR, USTED YA ES ADMIN EN ESTE GRUPO*'
 try {  
 await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 } catch {
