3324e03610a3e2219c946dbc98619bd67aa60bef
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-27 12:01:11 -0600

    Update game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -5,19 +5,19 @@ let handler = async (m, { conn, usedPrefix }) => {
 conn.tekateki = conn.tekateki ? conn.tekateki : {}
 let id = m.chat
 if (id in conn.tekateki) {
-conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
+conn.reply(m.chat, '*⚠️ TODAVÍA HAY UN ACERTIJO SIN RESPONDER EN ESTE CHAT*', conn.tekateki[id][0])
 throw false
 }
 let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
-m.react('🤔') 
+m.react('🤓') 
 await conn.sendPresenceUpdate('composing', m.chat)
 let json = tekateki[Math.floor(Math.random() * tekateki.length)]
 let _clue = json.response
 let clue = _clue.replace(/[A-Za-z]/g, '_')
 let caption = `
-ⷮ *${json.question}*
-*• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
-*• Bono:* +${poin} Exp
+🥏 *${json.question}*
+*⏰ Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*🍏 Bono:* +${poin} Exp
 `.trim()
 conn.tekateki[id] = [
 await conn.reply(m.chat, caption, m), json, 
