31d8fd999a3e48c07e334e74909c422019ea5d6d
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-06-27 12:24:16 -0600

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,12 +1,12 @@
-
 let handler = async (m, { conn, text }) => {
 	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`✳️ No estas en partida de TicTacToe 🎮 `, m)
+if (room == undefined) return conn.reply(m.chat,`*⚠️ NO ESTAS EN UNA PARTIDA DE TICTACTOE*`, m)
 delete conn.game[room.id]
-await conn.reply(m.chat, `╭─╮─᤻─᳒─᤻᳒「░⃟⃜🍭ꪳ۫₎۬〬${vs} ░⃟⃜🐾⁩」\n├❥ᰰຼ Se reinició la sesión de *tictactoe 🎮*\n*╰┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭۫*`, m)
+await conn.reply(m.chat, `🎮 *${wm}* 🎮\nSe reinició la sesión de *tictactoe 🎮*`, m)
 }
 handler.help = ['delttt']
 handler.tags = ['game']
 handler.command = ['delttc', 'delttt', 'delxo']
 handler.register = true 
+handler.group = true
 export default handler
