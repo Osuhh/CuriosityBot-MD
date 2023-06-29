cc80046bec3a604ae54dfe284d3b416f676a47d6
LoliBot <137656601+undefinidoo@users.noreply.github.com>
2023-06-29 18:37:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 console.log('🚀 Iniciando...')
-
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from "module";
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-
-// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
-const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname) 
+const { name, author } = require(join(__dirname, './package.json')) 
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
@@ -29,27 +26,27 @@ say(`'${name}' By @Azami19._`, {
 
 var isRunning = false
 /**
- * Start a js file
- * @param {String} file `path/to/file`
- */
+* Start a js file
+* @param {String} file `path/to/file`
+*/
 function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
+if (isRunning) return
+isRunning = true
+let args = [join(__dirname, file), ...process.argv.slice(2)]
+
+say('Ajuste la pantalla para escanear el codigo QR', {
+font: 'console',
     align: 'center',
-    gradient: ['blue', 'red']
-  })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  })
-  let p = fork()
-  p.on('message', data => {
-    console.log('[RECEIVED]', data)
-    switch (data) {
-      case 'reset':
+    gradient: ['blue', 'red']})
+  
+setupMaster({
+exec: args[0],
+args: args.slice(1), })
+let p = fork()
+p.on('message', data => {
+console.log('[RECEIVED]', data)
+switch (data) {
+case 'reset':
 p.process.kill()
 isRunning = false
 start.apply(this, arguments)
@@ -59,7 +56,7 @@ p.send(process.uptime())
 break }})
 p.on('exit', (_, code) => {
 isRunning = false
-console.error('⚠️ Ocurrió un error inesperado️', code)
+console.error('⚠️ Ocurrió un error inesperado:', code)
   
 p.process.kill()
 isRunning = false
@@ -71,13 +68,8 @@ process.exit(1)
 process.exit()
 }
 })
-  //----
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
-  // console.log(p)
-}
-
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test'])
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim())})}
 start('main.js')
