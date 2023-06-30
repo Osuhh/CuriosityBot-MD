589f63d647c822ba177e7bd49670fd6e066ed841
LoliBot <137656601+undefinidoo@users.noreply.github.com>
2023-06-29 22:55:38 -0300

    Add files via upload

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,14 +1,17 @@
-let instagramGetUrl = require('instagram-url-direct')
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = (await instagramGetUrl(args[0])).url_list[0]
-
-    conn.sendFile(m.chat, results, 'ig.mp4', `*Instagram Downloader*`, m)
+    if (!args[0]) throw `·˚ ༘₊· ͟͟͞͞꒰➳ 𝚄𝚜𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘\n𝙴𝚓𝚎𝚖𝚙𝚕𝚘: *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.react(rwait)
+    m.reply('Calmao 😎\n*Estoy descargando tu post 🔄*\n\nAguarde un momento, por favor')
+    const results = await instagramdl(args[0])
+        .catch(async _ => await instagramdlv2(args[0]))
+        .catch(async _ => await instagramdlv3(args[0]))
+        .catch(async _ => await instagramdlv4(args[0]))
+    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `? *Url:* ${url}`, m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(Instagram|ig|igdl)$/i
+handler.command = /^(ig(dl)?)$/i
 handler.diamond = true
-
-export default handler 
+export default handler
