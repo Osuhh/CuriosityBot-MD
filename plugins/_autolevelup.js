b76e7cf52cdd6cce979e5ac7ac3b4039eff8dfa1
LoliBot <137656601+undefinidoo@users.noreply.github.com>
2023-06-30 16:51:03 -0300

    Add files via upload

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -8,7 +8,7 @@ export async function before(m, { conn }) {
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
-    user.role = global.rpg.role(user.level).name
+  //  user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
     m.reply(`*「 FELICIDADES LEVEL UP 🆙🥳 」*\n\nFelicidades subiste de nivel sigue asi 👏\n\n☞ NIVEL ANTERIOR: ${before}\n☞ NIVEL ACTUAL: ${user.level}\n☞ RANGO : ${user.role}\n☞ FECHA: ${new Date().toLocaleString('id-ID')}\n\n\n_*Para ver tu XP en tiempo real coloca el comando #level*_`.trim())
    /* conn.sendButton(m.chat, '🤑 ' + wm, `╭━─━─━─≪🔆≫─━─━─━╮
