c5c21f3f896aa30776eb7d0fb2b99ae4b71243fe
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-01 13:05:56 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -70,7 +70,7 @@ let td = `${pickRandom([d1,d2,d3,d4,d5,d6])}`
     }
     await conn.reply(m.chat, '*Enviando el menu . . .*', ftrol) 
 m.react('🚀')     
-let menu = `╭━〘 CuriosityBot-MD ⁩〙━⊷
+let menu = `╭━( CuriosityBot-MD )⁩━⊷
 
 *U S U A R I O*
 *Nombre:* ${name}
