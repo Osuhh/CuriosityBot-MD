a1e502519de5f992241850101d6d6ca5cb459936
undefinidoo <137656601+undefinidoo@users.noreply.github.com>
2023-07-06 19:15:45 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -158,7 +158,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     
     let pp = './storage/menus/Menu2.jpg'
      
-    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, nna)
+    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, nnn)
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
       ['⏍ Info', `${_p}botinfo`],
