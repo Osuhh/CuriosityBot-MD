ff79833db7e41f9dfad47fbaf3b9b9e137cd42c2
undefinidoo <137656601+undefinidoo@users.noreply.github.com>
2023-07-07 18:21:11 -0300

    Add files via upload

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -3,8 +3,9 @@ let handler = m => m
 
 handler.before = async (m) => {
 let chat = global.db.data.chats[m.chat]
-if (chat.simi) {
-if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+if (chat.simi && !chat.isBanned && !m.fromMe) {
+if (m.text.startsWith(Prefix)) return
+//if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 let textodem = m.text  
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
