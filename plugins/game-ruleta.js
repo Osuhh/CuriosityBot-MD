7c9abbc3a121d4d7267ce013386c3c0bd2fe63f9
undefinidoo <137656601+undefinidoo@users.noreply.github.com>
2023-07-09 14:41:44 -0300

    Add files via upload

--- plugins/game-ruleta.js
+++ plugins/game-ruleta.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, usedPrefix, args }) => {
 
 let amount = args[0]
 
-if (global.db.data.users[m.sender].coins < amount) {
-  return m.reply('No tienes suficientes monedas para realizar esa apuesta.')
+if (global.db.data.users[m.sender].exp < amount) {
+  return m.reply('No tienes suficientes exp para realizar esa apuesta.')
 }
 
 let payout = 0;
-const randomNumber = Math.floor(Math.random() * 36) + 1; // Generar un número aleatorio entre 1 y 36
+const randomNumber = Math.floor(Math.random() * 850) + 1; // Generar un número aleatorio entre 1 y 850
 let type = args[1].toLowerCase()
 switch (type) {
   case '1st':
@@ -19,11 +19,11 @@ switch (type) {
     return m.reply('El espacio apostado es inválido.')
 }
 if (payout > 0) {
-  global.db.data.users[m.sender].coins += payout // Gano la apuesta
-  m.reply(`¡Ganaste ${payout} 🪙! Ahora tienes un total de ${global.db.data.users[m.sender].coins} monedas.`)
+  global.db.data.users[m.sender].exp += payout // Gano la apuesta
+  m.reply(`¡Ganaste ${payout} 🪙! Ahora tienes un total de ${global.db.data.users[m.sender].exp} exp.`)
 } else {
-  global.db.data.users[m.sender].coins -= amount; // Perdio la apuesta
-  m.reply(`Perdiste ${amount} monedas. Ahora tienes un total de ${global.db.data.users[m.sender].coins} monedas.`)
+  global.db.data.users[m.sender].exp -= amount; // Perdio la apuesta
+  m.reply(`Perdiste ${amount} monedas. Ahora tienes un total de ${global.db.data.users[m.sender].exp} monedas.`)
    }
   
 }
