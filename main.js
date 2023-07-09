1e9322ef27a59045925dc54515d2da80e3a584c5
CuriosityBot-MD <137133333+Undefined17@users.noreply.github.com>
2023-07-09 00:32:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ function clearTmp() {
 
 setInterval(async () => {
     backupCreds()
-    console.log(chalk.whiteBright(`\n▣────────[ BACKUP_CREDS ]───────────···\n│\n▣─❧ RESPALDO EXITOSO ✅\n│\n▣────────────────────────────────────···\n`))
+    console.log(chalk.whiteBright(`BACKUP_CREDS │ RESPALDO EXITOSO`))
     }, 15 * 60 * 1000)
 setInterval(async () => {
     clearTmp()
@@ -498,22 +498,22 @@ setInterval(async () => {
     }, 1000 * 60 * 3)
 setInterval(async () => {
      purgeSession()
-    console.log(chalk.cyanBright(`\n▣────────[ AUTOPURGESESSIONS ]───────────···\n│\n▣─❧ ARCHIVOS ELIMINADOS ✅\n│\n▣────────────────────────────────────···\n`))
+    console.log(chalk.yellowBright(`AUTOPURGESESSIONS │ ARCHIVOS ELIMINADOS`))
     }, 1000 * 60 * 60)
 setInterval(async () => {
       purgeSessionSB()
-     console.log(chalk.cyanBright(`\n▣────────[ AUTO_PURGE_SESSIONS_SUB-BOTS ]───────────···\n│\n▣─❧ ARCHIVOS ELIMINADOS ✅\n│\n▣────────────────────────────────────···\n`))
+     console.log(chalk.yellowBright(`AUTO_PURGE_SESSIONS_SUB-BOTS │ ARCHIVOS ELIMINADOS`))
     }, 1000 * 60 * 60)
 setInterval(async () => {
      purgeOldFiles()
-    console.log(chalk.cyanBright(`\n▣────────[ AUTO_PURGE_OLDFILES ]───────────···\n│\n▣─❧ ARCHIVOS ELIMINADOS ✅\n│\n▣────────────────────────────────────···\n`))
+    console.log(chalk.yellowBright(`AUTO_PURGE_OLDFILES │ ARCHIVOS ELIMINADOS`))
     }, 1000 * 60 * 60)
 
-//  if (!s.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
-//  if (s.ffmpeg && !s.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
-//  if (!s.convert && !s.magick && !s.gm) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
+  if (!s.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
+  if (s.ffmpeg && !s.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
+  if (!s.convert && !s.magick && !s.gm) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
 
 
 _quickTest()
-.then(() => conn.logger.info(`\n\n[_>] Prueba rápida realizada ✓\n`))
+.then(() => conn.logger.info(`\n\nC A R G A N D O ⚡\n`))
 .catch(console.error)
